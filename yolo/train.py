caf79170bdf740c2144951e50ed68a297fbc92f6
DOODODADA <apeter9401@gmail.com>
2023-04-17 12:55:25 +0800

    add best record args.yaml

--- yolo/train.py
+++ yolo/train.py
@@ -6,5 +6,5 @@ yaml_loc = "../datasets/datasets.yaml"
 model = YOLO('yolov8x.pt')  # load a pretrained model (recommended for training)
 
 # # Use the model
-results = model.train(data=yaml_loc, epochs=200, batch=16, save_period=5)  # train the model
+results = model.train(data=yaml_loc, epochs=500, batch=30, seed=323, lrf=1e-4, lr0=5e-2, warmup_epochs = 10)  # train the model
 results = model.val()  # evaluate model performance on the validation set
